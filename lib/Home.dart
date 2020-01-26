ac19566d3d9fd09328269cfc3a2335439c110ef1
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2020-01-25 23:15:04 -0300

    Utilzando Shared preferences para salvar dados no dispositivo

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,9 +9,29 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
 
   String _valorInformado = "Nada salvo!";
+  var _controller = TextEditingController();
 
-  _salvar(){}
-  _ler(){}
+  _salvar() async{
+    final preferencias = await SharedPreferences.getInstance();
+    await preferencias.setString("valorInformado", _controller.text);
+    setState(() {
+      _valorInformado = _controller.text;
+    });
+
+  }
+  _ler() async{
+    final preferencias = await SharedPreferences.getInstance();
+    setState(() {
+      _valorInformado = preferencias.getString("valorInformado") ?? "Nada salvo!";
+    });
+  }
+  _remover() async{
+    final preferencias = await SharedPreferences.getInstance();
+    preferencias.remove("valorInformado");
+    setState(() {
+      _valorInformado = "Nada salvo!";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +49,7 @@ class _HomeState extends State<Home> {
             ),
             TextField(
               keyboardType: TextInputType.text,
+              controller: _controller,
               style: TextStyle(
                 fontSize: 15
               ),
@@ -51,12 +73,25 @@ class _HomeState extends State<Home> {
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(top: 16),
+                  padding: EdgeInsets.only(top: 16, right: 16),
                   child: RaisedButton(
                     color: Colors.lightGreen,
                     textColor: Colors.white,
                     onPressed: _ler,
                     child: Text("Ler",
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold
+                      ),),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: RaisedButton(
+                    color: Colors.lightGreen,
+                    textColor: Colors.white,
+                    onPressed: _remover,
+                    child: Text("Remover",
                       style: TextStyle(
                           fontSize: 15,
                           fontWeight: FontWeight.bold
